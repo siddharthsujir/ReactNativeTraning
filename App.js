fa396d9a3400ea14a3284027e1bd0207d47799bd
unknown <shivaa.siddharth@gmail.com>
2020-07-19 22:01:12 +0530

    Button, TextInput, Flex

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!' )
-  const[count, newCount] = useState(0)
+
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Text>{count}</Text>
-      <Button title="Change Text" onPress={()=>setOutputText('Hello to your firstChange')} ></Button>
-      <Button title="Press Count" onPress = {()=> newCount(count+1)} ></Button>
-      <Button title="Reset Count" onPress = {()=> newCount(count*0)} ></Button>
+    <View style={{padding: 50}}>
+
+      <View style={{flexDirection: 'row', justifyContent:'space-between', alignItems:'center'}}> 
+          <TextInput placeholder="Enter a Value " style={{width: '80%', borderBottomColor: 'black', borderWidth: 1, padding:10}}/>
+          <Button title="Add"></Button>
+      </View>
+      <View>
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
